97aa49e268cfa4352290e06b1f23bd6c2be4e555
williammartinsmario <wmartins-mario@hotmail.com>
2026-03-26 13:07:02 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-
-const CACHE='wm-discador-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/', '/index.html'])));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
